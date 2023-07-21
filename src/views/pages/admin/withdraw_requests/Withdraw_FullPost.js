d3ddce800c481446ef329077e4e1887c12592e5c
Kinjal Madhu <kinjal.m@communicationcrafts.com>
2023-07-21 12:37:38 +0530

    pdf and doc images identifier in withdraw detail

--- src/views/pages/admin/withdraw_requests/Withdraw_FullPost.js
+++ src/views/pages/admin/withdraw_requests/Withdraw_FullPost.js
@@ -354,39 +354,41 @@ const Fullpage = (props) => {
                                         cursor: "pointer",
                                       }}
                                     >
-                                      <svg
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                      >
-                                        <path
-                                          d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
-                                          stroke="black"
-                                          strokeWidth="2"
-                                          strokeLinecap="round"
-                                          strokeLinejoin="round"
-                                        />
-                                        <path
-                                          d="M7 10L12 15L17 10"
-                                          stroke="black"
-                                          strokeWidth="2"
-                                          strokeLinecap="round"
-                                          strokeLinejoin="round"
-                                        />
-                                        <path
-                                          d="M12 15V3"
-                                          stroke="black"
-                                          strokeWidth="2"
-                                          strokeLinecap="round"
-                                          strokeLinejoin="round"
-                                        />
-                                      </svg>
+                                      <p className="bg-extension">
+                                        <svg
+                                          width="24"
+                                          height="24"
+                                          viewBox="0 0 24 24"
+                                          fill="none"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                          <path
+                                            d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
+                                            stroke="black"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                          />
+                                          <path
+                                            d="M7 10L12 15L17 10"
+                                            stroke="black"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                          />
+                                          <path
+                                            d="M12 15V3"
+                                            stroke="black"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                          />
+                                        </svg>
+                                      </p>
                                     </label>
                                   </li>
                                   <li className="w-75">
-                                    <div className="w-100 h-100 d-flex flex-column align-items-start overflow-auto pl-3">
+                                    <div className="w-100 h-100 d-flex flex-column align-items-start overflow-auto pl-3 bg-extension">
                                       {[...recieptFiles].map((item) => {
                                         return (
                                           <p className="p-0 m-0">{item.name}</p>
@@ -405,7 +407,15 @@ const Fullpage = (props) => {
                               <ul className="pl-0">
                                 {receipt_images.map((item, index) => {
                                   const file = receipt_images?.[index];
-                                  // const extension = file.receipt?.split(".")?.[1] || "";
+                                  const extension =
+                                    file.receipt?.split(".")?.[1] || "";
+                                  const imgExtensions = [
+                                    "png",
+                                    "jpg",
+                                    "jpeg",
+                                    "heif",
+                                    "heic",
+                                  ];
 
                                   return (
                                     <li key={item.id}>
@@ -415,35 +425,56 @@ const Fullpage = (props) => {
                                             handleDownloadReciept(file.id)
                                           }
                                         >
-                                          <svg
-                                            width="24"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                          >
-                                            <path
-                                              d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
-                                              stroke="black"
-                                              strokeWidth="2"
-                                              strokeLinecap="round"
-                                              strokeLinejoin="round"
+                                          {extension === "pdf" ? (
+                                            <img
+                                              src="/assets/ic_pdf.svg"
+                                              alt="pdf"
                                             />
-                                            <path
-                                              d="M7 10L12 15L17 10"
-                                              stroke="black"
-                                              strokeWidth="2"
-                                              strokeLinecap="round"
-                                              strokeLinejoin="round"
+                                          ) : extension === "doc" ? (
+                                            <img
+                                              src="/assets/ic_document.svg"
+                                              alt="doc"
                                             />
-                                            <path
-                                              d="M12 15V3"
-                                              stroke="black"
-                                              strokeWidth="2"
-                                              strokeLinecap="round"
-                                              strokeLinejoin="round"
+                                          ) : imgExtensions.includes(
+                                              extension
+                                            ) ? (
+                                            <img
+                                              src="/assets/ic_image_receipt.svg"
+                                              alt="image_type"
                                             />
-                                          </svg>
+                                          ) : (
+                                            <p className="bg-extension">
+                                              <svg
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                              >
+                                                <path
+                                                  d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
+                                                  stroke="black"
+                                                  strokeWidth="2"
+                                                  strokeLinecap="round"
+                                                  strokeLinejoin="round"
+                                                />
+                                                <path
+                                                  d="M7 10L12 15L17 10"
+                                                  stroke="black"
+                                                  strokeWidth="2"
+                                                  strokeLinecap="round"
+                                                  strokeLinejoin="round"
+                                                />
+                                                <path
+                                                  d="M12 15V3"
+                                                  stroke="black"
+                                                  strokeWidth="2"
+                                                  strokeLinecap="round"
+                                                  strokeLinejoin="round"
+                                                />
+                                              </svg>
+                                            </p>
+                                          )}
                                         </button>
                                       )}
                                     </li>
