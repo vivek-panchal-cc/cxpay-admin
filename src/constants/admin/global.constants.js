0fd133c1eb4930d7fc0c1e38b9db96ae8c3cda51
Vivek Panchal <vivek.p@communicationcrafts.com>
2025-04-02 10:54:54 +0530

    jars to sub-accounts

--- src/constants/admin/global.constants.js
+++ src/constants/admin/global.constants.js
@@ -39,7 +39,7 @@ export const globalConstants = {
   EXPORT_REPORT: "Export report",
   EXPORT_TRANSACTION_DATA: "Export transaction data",
   EXPORT_MERCHANT_DATA: "Export merchant data",
-  EXPORT_JARS_DATA: "Export jars data",
+  EXPORT_JARS_DATA: "Export sub-accounts data",
   EXPORT_AGENT_DATA_INDEX: "Export agent details report",
   VIEW_WITHDRAW_DETAILS: "View withdraw request detail",
   VIEW_MANUAL_TOPUP_DETAILS: "View manual top up details",
